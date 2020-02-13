7103feff25e45db688fe7c599abafaa32edde3e0
Melvin <peepersm@gmail.com>
2020-02-12 23:00:51 -0500

    printing

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,15 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+# https://docs.python.org/3/library/stdtypes.html?highlight=printf#old-string-formatting
+# https://www.geeksforgeeks.org/python-output-formatting/
+print('x is %d, y is %f, z is "%s"' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+# https://www.digitalocean.com/community/tutorials/how-to-use-string-formatters-in-python-3
+string_method = 'x is {0}, y is {1}, z is "{2}"'
+print(string_method.format(x, y, z))
+
 
 # Finally, print the same thing using an f-string
+# print()
