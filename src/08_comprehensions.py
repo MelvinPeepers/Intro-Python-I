cfe7c18e612f23a1d1a9d751fac921118cc448e9
Melvin <peepersm@gmail.com>
2020-02-15 13:40:02 -0500

    comprehensions.py completed

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -7,35 +7,38 @@ the list should be populated.
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
 """
+# https://www.digitalocean.com/community/tutorials/understanding-list-comprehensions-in-python-3
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [y for y in range(5)]
 
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
+# https://ehmatthes.github.io/pcc/solutions/chapter_4.html#4-8-cubes
 
-y = []
+y = [y**3 for y in range(0, 10)]
 
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
-
+# http://www.endmemo.com/python/lowercase.php
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [y.upper() for y in a]
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
+# https://www.geeksforgeeks.org/python-program-to-print-even-numbers-in-a-list/
 
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
